f1fe955e4f36eddeada4baf46411204266784faf
Morten Joelsen <bonzai.slasher@gmail.com>
2017-08-31 14:57:58 +0200

    at last

--- Scripts/GettingStarted.py
+++ Scripts/GettingStarted.py
@@ -27,16 +27,38 @@ def getMeasurementsBetweenDates(start, end, lane, cartype, name):
     query.append({'stationName': {'$regex': name}})
     query.append({'lane': {'$eq': int(lane)}})
     query.append({'carType': {'$eq': int(cartype)}})
-    query.append({'dateTime': {'$gte': datetime.datetime(2017, 4, 30, 21, 35, 17)}})
-    query.append({'dateTime': {'$lte': datetime.datetime(2017, 4, 30, 22, 35, 17)}})
+    #query.append({'dateTime': {'$gte': datetime.datetime(2017, 4, 30, 21, 35, 17)}})
+    #query.append({'dateTime': {'$lte': datetime.datetime(2017, 4, 30, 22, 35, 17)}})
+    #splitdaytime = str(start).split(" ")
+    #splitday = splitdaytime[0].split("-")
+    #splittime = splitdaytime[1].split(":")
+    #datefrom = datetime.datetime(int(splitday[0]), int(splitday[1]), int(splitday[2]), int(splittime[0]), int(splittime[1]), int(splittime[2]))
+    #query.append({'dateTime': {'$gte': datefrom}})
+    query.append({'dateTime': {'$gte': datetime.datetime.strptime(start, '%Y-%m-%d %H:%M:%S')}})
+    query.append({'dateTime': {'$lte': datetime.datetime.strptime(end, '%Y-%m-%d %H:%M:%S')}})    
+    #splitdaytime = str(end).split(" ")
+    #splitday = splitdaytime[0].split("-")
+    #splittime = splitdaytime[1].split(":")
+    #dateto = datetime.datetime(int(splitday[0]), int(splitday[1]), int(splitday[2]), int(splittime[0]), int(splittime[1]), int(splittime[2]))
+    #query.append({'dateTime': {'$lte': dateto}})
     fromdb = col.find({"$and": query})
-    #fromdb = pd.read_json(json.dumps(col.find({"$and": query}).limit(5)), default=json_util.default)
-    #print(fromdb.head())
     for x in fromdb:
-        found[i] = datetime.datetime(x['dateTime'])
+        found[i] = x
+        found[i]['dateTime'] = str(x['dateTime'])
+        found[i]['_id'] = str(x['_id'])
         i = i+1
-    
-    return json.dumps(found[0], default=json_util.default)
+
+    res = json.dumps(found, default=json_util.default)
+    # Pandas efterbehandling af data
+    data = pd.read_json(res)
+    data = data.transpose()
+    print(data.columns)
+    data.dateTime = pd.to_datetime(data.dateTime)
+    print(data.dtypes)
+    print(data.columns)
+    print(data.speed.count())
+    return res
+    #return json.dumps(found, default=json_util.default)
 
 @app.route("/GetStation/<name>")
 def getOneStation(name):
