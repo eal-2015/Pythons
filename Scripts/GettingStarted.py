962cc493fc13d01c5a0e2457d78125ebc0da7c73
Morten Joelsen <bonzai.slasher@gmail.com>
2017-08-31 12:17:46 +0200

    datetime errors

--- Scripts/GettingStarted.py
+++ Scripts/GettingStarted.py
@@ -1,8 +1,10 @@
 import json
 from pymongo import MongoClient
 import pymongo
+import datetime
 from bson import ObjectId, json_util
 from flask import Flask
+import pandas as pd
 app = Flask(__name__)
 '''
 from collections import OrderedDict
@@ -12,26 +14,29 @@ import bson.regex
 import bson.son
 '''
 
-client = MongoClient("10.190.80.25")
-db = client.Trafik_DB
+mongoIp = "10.190.80.25"
 
-@app.route("/MeasurementsBetweenDates/<start>/<end>/<lane>/<cartype>/<stationname>")
-def getMeasurementsBetweenDates(start, end, lane, cartype, stationname):
+@app.route("/MeasurementsBetweenDates/<start>/<end>/<lane>/<cartype>/<name>")
+def getMeasurementsBetweenDates(start, end, lane, cartype, name):
     col = connectToMeasurements()
     found = {}
     i = 0
-    #fromdb = col.find({'$text': {'$search': stationname}} and {'$carType':  {'$search': cartype}})
-    query = {'stationName': stationname}
-    #query['lane'] = lane
-    #query['carType'] = cartype
-    #query = [{'stationName': stationname}]
-    #query.append({'lane': lane})
-    #query.append({'carType': cartype})
-    fromdb = col.find(query)
+    #fromdb = col.find({'$text': {'$search': name}}).limit(15) #<-- slow?
+    #fromdb = col.find({'stationName': {"$regex": name}}).limit(15) #<-- fast?
+    query = []
+    query.append({'stationName': {'$regex': name}})
+    query.append({'lane': {'$eq': int(lane)}})
+    query.append({'carType': {'$eq': int(cartype)}})
+    query.append({'dateTime': {'$gte': datetime.datetime(2017, 4, 30, 21, 35, 17)}})
+    query.append({'dateTime': {'$lte': datetime.datetime(2017, 4, 30, 22, 35, 17)}})
+    fromdb = col.find({"$and": query})
+    #fromdb = pd.read_json(json.dumps(col.find({"$and": query}).limit(5)), default=json_util.default)
+    #print(fromdb.head())
     for x in fromdb:
-        found[i] = x
+        found[i] = datetime.datetime(x['dateTime'])
         i = i+1
-    return json.dumps(found, default=json_util.default)
+    
+    return json.dumps(found[0], default=json_util.default)
 
 @app.route("/GetStation/<name>")
 def getOneStation(name):
@@ -65,12 +70,12 @@ class JSONEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, o)
 
 def connectToStation():
-    client = MongoClient("10.190.80.25")
+    client = MongoClient(mongoIp)
     db = client.Trafik_DB
     return db.Stations
 
 def connectToMeasurements():
-    client = MongoClient("10.190.80.25")
+    client = MongoClient(mongoIp)
     db = client.Trafik_DB
     return db.Measurements
 
