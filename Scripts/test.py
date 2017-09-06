dd2a9f6bb604c2eb38fef6bac986e3d1eeab6996
Morten Joelsen <bonzai.slasher@gmail.com>
2017-09-06 10:46:24 +0200

    perfect test

--- Scripts/test.py
+++ Scripts/test.py
@@ -1,16 +1,13 @@
-import json
-from pymongo import MongoClient
-import pymongo
-import datetime
-from bson import ObjectId, json_util
-import pandas as pd
-import time
-import cgitb
-import cgi
+import json #used for converting to json
+from pymongo import MongoClient #used for connecting to mongodb
+import pymongo #used for interacting with mongodb
+from bson import json_util #used with json to make sure we got the format right
+import cgitb #for exception handling
+import cgi #for getting the arguments in the url
 
-cgitb.enable()
+cgitb.enable() #used for handling exceptions
 
-form = cgi.FieldStorage()
+args = cgi.FieldStorage() #gets a dictionary of the arguments like: {'argument key': 'argument value'}
 
 mongoIp = "localhost"
 
@@ -24,15 +21,25 @@ def connectToMeasurements():
     db = client.Trafik_DB
     return db.Measurements
 
+def fixStation(station):
+    station['_id'] = str(station['_id'])
+    station['installed'] = str(station['installed'])
+    return station
+
+def fixMeasurement(measurement):
+    measurement['_id'] = str(measurement['_id'])
+    measurement['dateTime'] = str(measurement['dateTime'])
+    return measurement
+
 def getOneStation(name):
     col = connectToStation()
     found = {}
     i = 0
     fromdb = col.find({'name': name})
     for x in fromdb:
-        found[i] = x
+        found[i] = fixStation(x)
         i = i+1
-    print(json.dumps(found, default=json_util.default))
+    return json.dumps(found, default=json_util.default)
 
 def getAllStations():
     col = connectToStation()
@@ -40,11 +47,17 @@ def getAllStations():
     tosend = {}
     i = 0
     for item in found:
-        tosend[i] = item
+        tosend[i] = fixStation(item)
         i = i+1
-    print(json.dumps(tosend, default=json_util.default))
+    return json.dumps(tosend, default=json_util.default)
 
-print('Content-Type: text/plain')
-print('')
-#getAllStations()
-getOneStation(str(form.getvalue('name')))
+def runMe(allArgs):
+    if len(allArgs) == 0: #if no args were sent
+        return getAllStations()
+    #if we have args use the name to get a station
+    return getOneStation(args.getvalue('name'))
+
+print('Content-Type: text/plain') #can be changed for what you like
+print('Access-Control-Allow-Origin: *') #needed
+print('') #needed
+print(runMe(args)) #run what ever function you want
